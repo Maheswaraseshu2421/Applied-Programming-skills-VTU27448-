6e54e606612c3045bce519d34af1a5c5ca080de7
Maheswaraseshu2421 <padalaseshu276@gmail.com>
2026-09-03 09:19:42 +0530

    Sorting colors

---
+++ UNIT 1/Session 3/Sort colour/Task 6.java
@@ -0,0 +1,31 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                // Swap 0 to the left
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } 
+            else if (nums[mid] == 1) {
+                // 1 is already in the correct middle section
+                mid++;
+            } 
+            else {
+                // Swap 2 to the right
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
