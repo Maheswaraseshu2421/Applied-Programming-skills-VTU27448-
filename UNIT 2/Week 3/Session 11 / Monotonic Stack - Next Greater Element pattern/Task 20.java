026464e0d9113bd283b74d039324163558a10b8a
Maheswaraseshu2421 <padalaseshu276@gmail.com>
2026-09-17 09:37:40 +0530

    dailyTemperatures using a monotonic stack

---
+++ UNIT 2/Week 3/Session 11 / Monotonic Stack - Next Greater Element pattern/Task 20.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+
+        // Store indices
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+
+            // Current temperature is warmer than the temperature
+            // at the index on top of the stack
+            while (!stack.isEmpty() &&
+                   temperatures[i] > temperatures[stack.peek()]) {
+
+                int prevIndex = stack.pop();
+
+                // Number of days to wait
+                answer[prevIndex] = i - prevIndex;
+            }
+
+            stack.push(i);
+        }
+
+        // Remaining indices have no warmer future day,
+        // so their answer remains 0.
+
+        return answer;
+    }
+}
